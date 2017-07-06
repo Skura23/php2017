566453eb2b697c93a11ff04d8d3208bf0e646af5
company account <776064932@qq.com>
2017-07-06 18:07:01 +0800

    configure blog assets

---
+++ php-blog/assets/js/application.js
@@ -0,0 +1,2 @@
+$(function(){if(typeof $.fn.scrollAppear==='function'){$('.appear').scrollAppear();}
+$('.fluidbox-trigger').fluidbox();$('.article-share a').on('click',function(){window.open($(this).attr('href'),'Share','width=200,height=200,noopener');return false;});});
