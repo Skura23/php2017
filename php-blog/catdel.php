76e97382773af499a402ff97e81efa5cc086d246
skura23 <776064932@qq.com>
2017-07-07 00:15:07 +0800

    add and modi blog cat

--- php-blog/catdel.php
+++ php-blog/catdel.php
@@ -5,5 +5,12 @@
 $cat_id = $_GET['cat_id'];
 echo $cat_id;
 
+//连接数据库
+	$conn = mysql_connect('localhost','root','');
+	mysql_query('use blog',$conn);
+	mysql_query('set names utf8');
 
+//检测 $cat_id是否为数字
+//检测 是否有这个id
+//检测 栏目下是否有文章
  ?>
