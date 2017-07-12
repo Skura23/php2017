0379cf767046b27ff1708e8cb968d19a15941e44
company account <776064932@qq.com>
2017-07-12 17:21:48 +0800

    blog

--- php-blog/artlist.php
+++ php-blog/artlist.php
@@ -13,7 +13,12 @@ $catname = array();
 foreach ($rs_cat as $k => $v) {
 	$catname[$v['cat_id']] = $v['catname'];
 }
-/*var_dump($catname);*/
+/*var_dump($rs);*/
+/*page all data arr*/
+$pArr = getPage($rs,5);
+$pDisArr = $pArr[$_GET['page']];
+var_dump($pDisArr);
+
 
 
 include ROOT.'/view/admin/artlist.html';
--- php-blog/lib/func.php
+++ php-blog/lib/func.php
@@ -44,40 +44,53 @@ function getRealIp(){
 
 /**
 * 生成分页代码
-* @param int $aNum 文章总数
-*	@param int $sNum 文章总数
+* @param int $arr 条目数组
+*	@param int $sNum 每页条目数
+* @return 键名为页码  值为当页条目数组 的数组
 */
-if (!isset($_GET['page'])) {
-	$_GET['page'] =0;
-}
+/*if (!isset($_GET['page'])) {
+	$_GET['page'] =1;
+}*/
 $arr = array('a','b','c','d','e','f',1,2);
 function getPage($arr,$sNum){
+	if (!isset($_GET['page'])) {
+		$_GET['page'] =1;
+	}
+	
 	$aNum = count($arr);
 	$pNum = ceil($aNum/$sNum);
 
 	for ($i=0,$start = 0; $i < $pNum; $i++) {
-		$start = $i*3;
+		$start = $i*$sNum;
 		if ($i == $pNum-1) {
 			$pArr[] = array_slice($arr,$start,$aNum-$start);
 		}else{
 			$pArr[] = array_slice($arr,$start,$sNum);
 		}
 	}
+	//使key由0始变为1始
+	foreach ($pArr as $key => $value) {
+		# code...
+		$_pArr[$key+1] = $value;
+	}
+	
+	$pArr = $_pArr;
 	return($pArr);
 }	
-$pArr = getPage($arr, 3);
+/*$pArr = getPage($arr, 3);
 $disArr = $pArr[$_GET['page']];
-echo $_GET['page'];
+echo $_GET['page'];*/
 
  ?>
 
- <body>
- 	<ul>
- 		<?php foreach ($disArr as $key => $value) { ?>
- 		<li><?php echo $value ?></li>
- 		<?php } ?>
- 	</ul>
- 	<?php foreach ($pArr as $key => $value) { ?>
- 	<a href="func.php?page=<?php echo $key; ?>"><?php echo $key+1 ?></a>
- 	<?php } ?>
- </body>
+<!-- test html -->
+<!--  <body>
+	<ul>
+		<?php foreach ($disArr as $key => $value) { ?>
+		<li><?php echo $value ?></li>
+		<?php } ?>
+	</ul>
+	<?php foreach ($pArr as $key => $value) { ?>
+	<a href="func.php?page=<?php echo $key; ?>"><?php echo $key ?></a>
+	<?php } ?>
+</body> -->
