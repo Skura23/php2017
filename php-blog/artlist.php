2054b6bc28008a46cf84f7b710b12bb0e6e99739
skura23 <776064932@qq.com>
2017-07-22 13:54:10 +0800

    blog modi

--- php-blog/artlist.php
+++ php-blog/artlist.php
@@ -7,17 +7,18 @@ if (!checkCookie()) {
 	header('Location: login.php');
 }
 
-$sql = "select * from art";
-mGetAll($sql);
+$sql = "select * from art ORDER BY art_id";
+//mGetAll($sql);
 
 
 $rs = mGetAll($sql);
+
 $rs_cat = mGetAll("select * from cat");
 $catname = array();
 foreach ($rs_cat as $k => $v) {
 	$catname[$v['cat_id']] = $v['catname'];
 }
-/*var_dump($rs);*/
+
 /*page all data arr*/
 $pArr = getPage($rs,5);
 $pDisArr = $pArr[$_GET['page']];
